2e31fa30125ff3b8108056b4b5359e7e0c93ba28
Your Name <131968076+nothing-reasonable@users.noreply.github.com>
2026-03-15 04:28:29 +0600

    media-service removed, will be handled inside content-service

--- media-service/src/main/java/com/example/mediaservice/repository/MediaAssetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mediaservice.repository;
-
-import com.example.mediaservice.model.MediaAsset;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MediaAssetRepository extends JpaRepository<MediaAsset, String> {
-}
